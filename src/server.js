df00e8a50b50a110261622bee8a5d1a762b4a262
Antonio Sabino <aslimajr@outlook.com>
2022-05-29 19:43:37 -0300

    Incluindo as rotas ao server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 const app = require('./api');
+const loginRoute = require('./routes/login');
+const useRoute = require('./routes/login');
+const categoriesRoute = require('./routes/login');
+const postRoute = require('./routes/login');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -9,9 +13,9 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use('/login');
-app.use('/user');
-app.use('/categories');
-app.use('/post');
+app.use('/login', loginRoute);
+app.use('/user', useRoute);
+app.use('/categories', categoriesRoute);
+app.use('/post', postRoute);
 
 app.listen(port, () => console.log('ouvindo porta', port));
