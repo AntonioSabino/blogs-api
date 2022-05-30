dc90b423139e3328d4cd054fc805fe129595afb8
Antonio Sabino <aslimajr@outlook.com>
2022-05-29 21:46:34 -0300

    Resolução requisito 03. Utilizando JWT para gerar token

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,4 +1,5 @@
 const { User } = require('../database/models');
+const generateJWT = require('../util/generateJWT');
 
 const validateLogin = async (req, res) => {
   const { email, password } = req.body;
@@ -6,6 +7,14 @@ const validateLogin = async (req, res) => {
   const user = await User.findOne({ where: { email, password } });
 
   if (!user) return res.status(400).json({ message: 'Invalid fields' });
+
+  const userData = user.dataValues;
+
+  const { password: passDB, ...userWithoutPass } = userData;
+
+  const token = generateJWT(userWithoutPass);
+
+  res.status(200).json({ token });
 };
 
 module.exports = { validateLogin };
